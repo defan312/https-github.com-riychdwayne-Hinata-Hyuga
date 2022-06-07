9da40df551e912f35b59fc62a175d07df8478b8f
defan312 <106823339+defan312@users.noreply.github.com>
2022-06-07 21:43:44 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -23,9 +23,9 @@
   global.prefa = ['','.','/','#']
 
   // Other
-  global.ownername = 'Riy' // Ganti Jadi Namamu
+  global.ownername = 'def' // Ganti Jadi Namamu
   global.owner = ['6281575886399','6281575886399','6285813334446'] // Kalo Mau Lebih Tambahin Aja
-  global.ownernomer = '6281575886399' // Owner Utama Istilahnya
+  global.ownernomer = '6282135905896' // Owner Utama Istilahnya
   global.botname = 'Hinata Hyuga' // Ganti Jadi Nama Botmu
   global.packname = 'Hinata Hyuga' // Sticker Weem
   global.author = 'Riy' // Sticker Weem
